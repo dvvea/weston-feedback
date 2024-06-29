a5588f92745bdcfc44b45ca29371f503c2ddc999
Weston <168985530+dvvea@users.noreply.github.com>
2024-06-29 17:12:01 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'weston'
+description 'https://westonxx.com'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+}
+
+client_script 'client/client.lua'
+server_script 'server/server.lua' 
