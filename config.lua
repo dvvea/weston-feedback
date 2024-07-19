729f3353d635cb8be266163f55bc4fef97ed593f
Weston <168985530+dvvea@users.noreply.github.com>
2024-07-19 18:39:13 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 Config = {}
 
-Config.DiscordWebhook = "https://discord.com/api/webhooks/1249439248071786607/oJywHr5W-spAolqzHX0uN-10i5U2NoAaPxQc2tqiIoELHHb3IiQIbkWtgYW7i-RvFkEe"
+Config.DiscordWebhook = ""
